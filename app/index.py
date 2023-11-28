df75286b26a373e74d46cd91f88cf44bd8020706
LTMHoang <letruongminhhoang2003@gmail.com>
2023-11-28 19:37:17 +0700

    moi day len

--- app/index.py
+++ app/index.py
@@ -10,15 +10,7 @@ import hashlib
 
 @app.route('/')
 def index():
-    kw = request.args.get('kw')
-    cate_id = request.args.get('cate_id')
-    page = request.args.get('page')
-
-    pros = dao.load_products(kw, cate_id, page)
-    num = dao.count_product()
-
-    return render_template('index.html', products=pros,
-                           pages=math.ceil(num/app.config["PAGE_SIZE"]))
+    return render_template('index.html')
 
 
 @app.route('/admin/login', methods=['post'])
@@ -27,47 +19,47 @@ def admin_login():
     request.form.get('password')
 
 
-@app.route('/api/cart', methods=['post'])
-def add_to_cart():
-    """
-    {
-        "cart": {
-            "1": {
-                "id": "1",
-                "name": "ABC",
-                "price": 12,
-                "quantity": 1
-            }
-        }
-    }
-    :return:
-    """
+# @app.route('/api/cart', methods=['post'])
+# def add_to_cart():
+#     """
+#     {
+#         "cart": {
+#             "1": {
+#                 "id": "1",
+#                 "name": "ABC",
+#                 "price": 12,
+#                 "quantity": 1
+#             }
+#         }
+#     }
+#     :return:
+#     """
+#
+#     data = request.json
+#
+#     cart = session.get('cart')
+#     if cart is None:
+#         cart = {}
+#
+#     id = str(data.get("id"))
+#     if id in cart:
+#         cart[id]['quantity'] += 1
+#     else:
+#         cart[id] = {
+#             "id": id,
+#             "name": data.get('name'),
+#             "price": data.get('price'),
+#             "quantity": 1
+#         }
+#
+#     session['cart'] = cart
+#
+#     return jsonify(utils.count_cart(cart))
 
-    data = request.json
 
-    cart = session.get('cart')
-    if cart is None:
-        cart = {}
-
-    id = str(data.get("id"))
-    if id in cart:
-        cart[id]['quantity'] += 1
-    else:
-        cart[id] = {
-            "id": id,
-            "name": data.get('name'),
-            "price": data.get('price'),
-            "quantity": 1
-        }
-
-    session['cart'] = cart
-
-    return jsonify(utils.count_cart(cart))
-
-
-@app.route('/cart')
-def cart():
-    return render_template('cart.html')
+# @app.route('/cart')
+# def cart():
+#     return render_template('cart.html')
 
 
 @login.user_loader
@@ -88,14 +80,14 @@ def login_admin():
     return redirect("/admin")
 
 
-@app.context_processor
-def common_response():
-    return {
-        'categories': dao.load_categories(),
-        'cart': utils.count_cart(session.get('cart'))
-    }
+# @app.context_processor
+# def common_response():
+#     return {
+#         'categories': dao.load_categories(),
+#         'cart': utils.count_cart(session.get('cart'))
+#     }
 
 
 if __name__ == '__main__':
-    from app import admin
+    # from app import admin
     app.run(debug=True)
