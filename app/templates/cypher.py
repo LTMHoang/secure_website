d52a393cd53adbdecc0fff3efde687ba73481749
minh sang <msang.nms@gmail.com>
2023-11-30 21:07:15 +0700

    cypher

--- app/templates/cypher.py
+++ app/templates/cypher.py
@@ -4,8 +4,10 @@ def mod_inverse(a, m):
             return i
     return -1
 
+
 def affine_encrypt(plaintext, a, b, m=26):
     ciphertext = ""
+
     for char in plaintext:
         if char.isalpha():
             x = ord(char.upper()) - 65
@@ -13,13 +15,18 @@ def affine_encrypt(plaintext, a, b, m=26):
             ciphertext += chr(y + 65)
         else:
             ciphertext += char
+
     return ciphertext
+
 
 def affine_decrypt(ciphertext, a, b, m=26):
     a_inv = mod_inverse(a, m)
+
     if a_inv == -1:
         return "a and m must be coprime for the cipher to work"
+
     plaintext = ""
+
     for char in ciphertext:
         if char.isalpha():
             y = ord(char.upper()) - 65
@@ -27,11 +34,13 @@ def affine_decrypt(ciphertext, a, b, m=26):
             plaintext += chr(x + 65)
         else:
             plaintext += char
+
     return plaintext
 
+
 if __name__ == "__main__":
-    i = "UQZK"
+    i = "uqzk"
     a = affine_decrypt(i, 17, 12, 26)
-    print(affine_encrypt("CONG",17,12,26))
+    print(affine_encrypt("cong",17,12,26))
     print(a)
 
