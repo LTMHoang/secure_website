f8a41e9ef9f27144c1d21f04aabf2f80660af0a8
TTHai-Git <110751427+TTHai-Git@users.noreply.github.com>
2023-12-03 09:00:53 +0700

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin, BaseView, expose
 from app import app, db, admin
-from app.models import Category, Product, UserRoleEnum
+from app.models import Category, Product, UserRoleEnum, HocVien, KhoaHoc, DangKy, LopHoc, Diem, GiaoVien
 from flask_login import current_user, logout_user
 from flask import redirect
 
@@ -24,6 +24,64 @@ class MyProductView(AuthenticatedAdminMV):
     column_editable_list = ['name', 'price']
     details_modal = True
     edit_modal = True
+
+
+class MyHocVienView(AuthenticatedAdminMV):
+    column_list = ['id', 'ho_ten', 'ngay_sinh','dia_chi','email']
+    can_export = True
+    column_searchable_list = ['name']
+    column_filters = ['name']
+    column_editable_list = ['ho_ten', 'ngay_sinh','dia_chi','email']
+    details_modal = True
+    edit_modal = True
+
+
+class MyKhoaHocView(AuthenticatedAdminMV):
+    column_list = ['id', 'ten_khoa_hoc', 'mo_ta','dang_kys','email']
+    can_export = True
+    column_searchable_list = ['name']
+    column_filters = ['name']
+    column_editable_list = ['name', 'price']
+    details_modal = True
+    edit_modal = True
+
+class MyDangKyView(AuthenticatedAdminMV):
+    column_list = ['id', 'hoc_vien_id', 'khoa_hoc_id','ngay_dang_ky']
+    can_export = True
+    column_searchable_list = ['name']
+    column_filters = ['name']
+    column_editable_list = ['name']
+    details_modal = True
+    edit_modal = True
+
+
+class MyLopHocView(AuthenticatedAdminMV):
+    column_list = ['id','khoa_hoc_id', 'phong_hoc', 'thoi_gian','phong_hoc','giao_vien_id']
+    can_export = True
+    column_searchable_list = ['khoa_hoc_id','phong_hoc','giao_vien_id']
+    column_filters = ['khoa_hoc_id','phong_hoc','giao_vien_id']
+    column_editable_list = ['khoa_hoc_id','phong_hoc','giao_vien_id']
+    details_modal = True
+    edit_modal = True
+
+class MyDiemView(AuthenticatedAdminMV):
+    column_list = ['id','hoc_vien_id', 'lop_hoc_id', 'diem_so']
+    can_export = True
+    column_searchable_list = ['hoc_vien_id', 'lop_hoc_id', 'diem_so']
+    column_filters = ['hoc_vien_id', 'lop_hoc_id', 'diem_so']
+    column_editable_list = ['hoc_vien_id','diem_so']
+    details_modal = True
+    edit_modal = True
+
+class MyGiaoVienView(AuthenticatedAdminMV):
+    column_list = ['id', 'ten_giao_vien']
+    can_export = True
+    column_searchable_list = ['ten_giao_vien']
+    column_filters = ['ten_giao_vien']
+    column_editable_list = ['ten_giao_vien']
+    details_modal = True
+    edit_modal = True
+
 
 
 class MyCategoryView(AuthenticatedAdminMV):
@@ -51,3 +109,9 @@ admin.add_view(MyCategoryView(Category, db.session))
 admin.add_view(MyProductView(Product, db.session))
 admin.add_view(StatsView(name='Thống kê báo cáo'))
 admin.add_view(LogoutView(name="Đăng xuất"))
+admin.add_view(MyHocVienView(HocVien, db.session, name='Quản Lý Học Viên'))
+admin.add_view(MyKhoaHocView(KhoaHoc, db.session, name='Quản Lý Khóa Học'))
+admin.add_view(MyDangKyView(DangKy, db.session, name='Quản Lý Đăng Ký Khóa Học'))
+admin.add_view((MyLopHocView(LopHoc, db.session, name='Quản Lý Lớp Học')))
+admin.add_view(MyDiemView(Diem, db.session, name='Quản Lý Điểm'))
+admin.add_view(MyGiaoVienView(GiaoVien, db.session, name='Quản Lý Giáo Viên'))
